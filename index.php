c6d4eaae47e9c61023f9865192a4e88a1498daea
FRANCISCO CAIK SANTANA FARIAS <61545160+caikfarias@users.noreply.github.com>
2022-05-26 08:02:41 -0400

    index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html lang="pt_br">
+<head>
+	<meta charset="utf-8">
+	<title>Projeto Login</title>
+	<link rel="stylesheet" href="CSS/estilo.css">
+</head>
+<body>
+	<div id="corpo-form">
+	<h1>Entrar</h1>
+	<form method="POST">
+		<input type="email" placeholder="Usuário" name="email">
+		<input type="passowrd" placeholder="Senha" name="senha">
+		<input type="submit" value="LOGIN">
+		<a href="cadastrar.php">Ainda não é inscrito?<strong>Cadastre-se!</strong></a>
+	</form>
+	</div>
+<?php
+?>
+</body>
+</html>
